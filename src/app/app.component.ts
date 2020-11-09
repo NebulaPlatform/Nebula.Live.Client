dc58fce539ad5e7736d1bb94f48ca7991435184f
Simone Pesce <s.pesce@k2play.it>
2020-11-10 00:44:43 +0100

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,8 @@ ZoomMtg.prepareJssdk();
 export class AppComponent implements OnInit {
   // setup your signature endpoint here: https://github.com/zoom/websdk-sample-signature-node.js
   signatureEndpoint = 'https://nebula-framework.herokuapp.com/';
+  // signatureEndpoint = 'http://localhost:4000/';
+
   apiKey = 'UAW5toDfSje2dkLZYUXDtQ';
   meetingNumber = 75598836366;
   role = 0;
@@ -24,15 +26,46 @@ export class AppComponent implements OnInit {
   passWord = 'dU8tEx';
 
   meetingId: number;
+  password: number;
+  joinUrl: string;
+  startUrl: string;
+  jwt: string;
 
   constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getJwToken();
+  }
+
+  getJwToken() {
+    this.httpClient
+      .get(this.signatureEndpoint + 'token', {})
+      .subscribe((res) => {
+        console.log(res);
+        this.jwt = String(res);
+      });
+  }
+
+  createMeeting() {
+    this.httpClient
+      .post(this.signatureEndpoint + 'create', {
+        token: this.jwt,
+      })
+      .subscribe((res) => {
+        console.log(res);
+        if (res) {
+          this.meetingId = res['id'];
+          this.password = res['password'];
+          this.joinUrl = res['join_url'];
+          this.startUrl = res['start_url'];
+        }
+      });
+  }
 
   getSignature() {
     this.httpClient
       .post(this.signatureEndpoint, {
-        meetingNumber: this.meetingNumber,
+        meetingNumber: this.meetingId,
         role: this.role,
       })
       .toPromise()
@@ -60,11 +93,11 @@ export class AppComponent implements OnInit {
 
         ZoomMtg.join({
           signature: signature,
-          meetingNumber: this.meetingNumber,
+          meetingNumber: this.meetingId,
           userName: this.userName,
           apiKey: this.apiKey,
           userEmail: this.userEmail,
-          passWord: this.passWord,
+          passWord: this.password,
           success: (success) => {
             console.log(success);
           },
